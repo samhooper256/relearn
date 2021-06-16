0d6b2dc1d1bba8b0841b6b81f906c22fe6b974ce
samhooper256 <samhooper256@gmail.com>
2021-06-15 19:30:25 -0500

    Added testing class for displaying math text.

--- AppChallenge2021/src/module-info.java
+++ AppChallenge2021/src/module-info.java
@@ -1,6 +1,13 @@
 open module appchallenge2021 {
 	
+	requires transitive java.desktop;
+	
+	requires transitive javafx.base;
 	requires transitive javafx.graphics;
-	requires transitive javafx.controls;
+	requires transitive jlatexmath;
+	requires transitive org.jfree.fxgraphics2d;
+	requires transitive javafx.swing;
+
+	exports base;
 	
 }
