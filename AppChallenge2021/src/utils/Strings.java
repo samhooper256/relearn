2d8f06cf73d10b21e007b6e9ab7d40fd6c15961a
samhooper256 <samhooper256@gmail.com>
2021-06-19 15:49:26 -0500

    Fixed typo in Strings.stripTrailing(str, char[]) documentation

--- AppChallenge2021/src/utils/Strings.java
+++ AppChallenge2021/src/utils/Strings.java
@@ -18,7 +18,7 @@ public final class Strings {
 	}
 	
 	/** Removes the longest substring of the given string that contains only characters in {@code chars} and
-	 * contains ends at the end of the string.*/
+	 * ends at the end of the string. */
 	public static String stripTrailing(String str, char... chars) {
 		int i = str.length();
 		while(i > 0 && Arrs.contains(chars, str.charAt(i - 1)))
