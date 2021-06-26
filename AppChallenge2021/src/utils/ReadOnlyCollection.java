82d2daea9f382282c9db5e034f9a74e404bfda76
samhooper256 <samhooper256@gmail.com>
2021-06-26 09:10:22 -0500

    Added clarifying documentation to utils.ReadOnlyCollection

--- AppChallenge2021/src/utils/ReadOnlyCollection.java
+++ AppChallenge2021/src/utils/ReadOnlyCollection.java
@@ -6,6 +6,8 @@ package utils;
 import java.util.*;
 
 /**
+ * <p>The {@link Iterator} returned by {@link #iterator()} must <em>not</em> support the
+ * {@link Iterator#remove()} operation.</p>
  * @author Sam Hooper
  *
  */
@@ -13,9 +15,21 @@ public interface ReadOnlyCollection<E> extends Iterable<E> {
 
 	boolean contains(Object o);
 	
-	boolean containsAll(Collection<?> c);
-	
 	int size();
+	
+	default boolean containsAll(Collection<?> objects) {
+		for(Object o : objects)
+			if(!contains(o))
+				return false;
+		return true;
+	}
+	
+	default boolean containsAll(Object... objects) {
+		for(Object o : objects)
+			if(!contains(o))
+				return false;
+		return true;
+	}
 	
 	default boolean isEmpty() {
 		return size() == 0;
