3cd4ff88d1f272340dca14a46025277193f3cee4
samhooper256 <samhooper256@gmail.com>
2021-07-01 15:51:16 -0500

    Updated WebTest.

--- AppChallenge2021/src/base/WebTest.java
+++ AppChallenge2021/src/base/WebTest.java
@@ -15,7 +15,7 @@ public class WebTest extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		WebView view = new WebView();
 		WebEngine engine = view.getEngine();
-		engine.loadContent("<html>2<sup>3</sup>=8</html>");
+		engine.loadContent("<html>2<sup>3</sup>=8<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><msqrt><mn>2</mn></msqrt></math></html>");
 		
 		Scene scene = new Scene(view);
 		
